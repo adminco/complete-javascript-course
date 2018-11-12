c0d43ec7632eb84adbbf5a30c7d03c5fef56366c
adminco.local <gozy_black@yahoo.com>
2018-11-12 08:43:43 -0500

    added react app to the directory

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,7 +1,26 @@
 // function Constructor
-const Person  = function () {
-    this.name = name;
-    this.age = age;
-}
+const Person = function() {
+  this.name = name;
+  this.age = age;
+};
 
-console.log('name');
+console.log("name");
+
+const personProto = {
+  name: "Gozy",
+  getAge: function() {
+    return this.name;
+  }
+};
+
+let myArray = [1, 2, 3, 4, 5];
+let newArray = myArray.map(function(i) {
+  return i ** 2;
+});
+
+console.log(newArray);
+
+let apocalypse = "";
+let task = !apocalypse ? "learn React.js" : "run around"; //Ternary operator
+
+console.log(task);
